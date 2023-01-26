f0a9fdf12fb25fe6a854a2ffb475bb0a5e74e817
dallinforsyth <dallfor192@gmail.com>
2023-01-26 14:08:34 -0600

    added tweets for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+account = Account.create!(name: "JohnHammons", email: "killbill@test.com")
+Tweet.create!(tweet: "I love the ninja turtles!", account_id: account.id)
+Tweet.create!(tweet: "Raphfiel is my favorite! and i cant spell", account_id: account.id)
+
+account = Account.create!(name: "BIllyJole", email: "sillybilly@test.com")
+Tweet.create!(tweet: "whats the reasoning John?", account_id: account.id)
